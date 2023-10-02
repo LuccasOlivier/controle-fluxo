f147921e3d90404d8c15be74e970f6433095d795
LuccasOlivier <lukashamashia@gmail.com>
2023-10-02 18:30:58 -0300

    update flies

---
+++ src/ParametrosInvalidosException.java
@@ -0,0 +1,7 @@
+// ParametrosInvalidosException.java
+public class ParametrosInvalidosException extends Exception {
+    public ParametrosInvalidosException(String mensagem) {
+        super(mensagem);
+    }
+}
+
